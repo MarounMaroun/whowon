18c4ab3a5fd14fedd2e060b09987614799857482
MarounMaroun <maroun.bassam@gmail.com>
2018-06-22 20:58:39 +0300

    Add "-c" flag to get number of times a specific team has won the WC

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,8 +1,14 @@
+import Utils.getWcData
 import scopt.OptionParser
 
-case class Config(year: Int = -1)
+case class Config(year: Int = -1, cups: String = "")
 
 object Parser {
+  /** Checks if a world cup occurred in the given year.
+    *
+    * @param year The given year.
+    * @return true if WC occurred, false otherwise.
+    */
   private def isWorldCupYear(year: Int): Boolean = {
     for (y <- Range(1934, 2018, 4)) {
       if (year == y) return true
@@ -10,11 +16,20 @@ object Parser {
     false
   }
 
-  val parser: OptionParser[Config] = new scopt.OptionParser[Config]("worldcup") {
-    opt[Int]('y', "year").required.action {
+  val parser: OptionParser[Config] = new scopt.OptionParser[Config]("whowon") {
+    opt[Int]('y', "year").action {
       (x, c) => c.copy(year = x)
     }.validate { year =>
       if (isWorldCupYear(year)) success else failure(s"No world cup occurred on $year")
     }.text("year of the world cup")
+
+    opt[String]('c', "cups").action {
+      (x, c) => c.copy(cups = x)
+    }.validate { team =>
+      getWcData.find(t => t.winner.equalsIgnoreCase(team)|| t.against.equalsIgnoreCase(team)) match {
+        case Some(_) => success
+        case _ => failure(s"$team didn't participate at any known world cup")
+      }
+    }.text("team you want to know how many times won the world cup")
   }
 }
